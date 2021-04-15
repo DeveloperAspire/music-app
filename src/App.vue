58df1de700db368726b963e2ee8f816d8fdc7201
Franklin <franklinokolie86@gmail.com>
2021-04-15 13:09:45 +0100

    Added some changes

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,9 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
-
+body{
+  background: #2c3e50;
+}
 #app {
   font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
